a2ef00732b3173377fcc2f1e9995177eb5e55bd5
paddyroddy <patrickjamesroddy@gmail.com>
2018-11-09 22:22:04 +0000

    travis test

--- install.sh
+++ install.sh
@@ -76,6 +76,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install nvim plugins
-nvim --headless +PlugInstall +qa &> /dev/null
+nvim --headless +PlugInstall +qa :0cquit
 
 source $HOME/.bashrc 2> /dev/null
