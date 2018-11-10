f04d6739bf003dad0fa8c50bd24d7db201eca5fa
paddyroddy <patrickjamesroddy@gmail.com>
2018-11-10 11:42:31 +0000

    final test

--- install.sh
+++ install.sh
@@ -76,12 +76,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install nvim plugins
-nvim --headless +PlugInstall +qa 2> /dev/null
+nvim +PlugInstall +qa
 
-source $HOME/.bashrc 2> /dev/null
-
-if [ $? -eq 0 ]; then
-    echo "success"
-else
-    echo >&2
-fi
+source $HOME/.bashrc
