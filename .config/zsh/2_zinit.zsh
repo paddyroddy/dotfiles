0299555a1cc8cdd62bf410faee2c290398149fb9
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-09 18:48:55 +0100

    Switch to `zinit`

---
+++ .config/zsh/2_zinit.zsh
@@ -0,0 +1,13 @@
+source $(brew --prefix)/opt/zinit/zinit.zsh
+
+# zsh plugins
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-syntax-highlighting
+
+# zsh snippets
+zinit snippet OMZP::git
+
+# load completions
+autoload -Uz compinit && compinit
+zinit cdreplay -q
