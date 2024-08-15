9c70d69a1468cfc6c2c7b3e84fdc1de6dbdc391a
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-15 10:04:31 +0100

    Turn off auto correction

--- .config/zsh/0_oh_my_zsh.zsh
+++ .config/zsh/0_oh_my_zsh.zsh
@@ -38,7 +38,7 @@ zstyle ':omz:update' mode disabled  # disable automatic updates
 DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-ENABLE_CORRECTION="true"
+# ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
