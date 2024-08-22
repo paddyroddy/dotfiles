9700140efe7a62c982b4bc677ecfbc22d4eeb0ae
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-22 16:20:56 +0100

    `zsh-autocomplete` config

--- .config/zsh/1_environment_variables_general.zsh
+++ .config/zsh/1_environment_variables_general.zsh
@@ -8,3 +8,7 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=cyan"
 # tmux plugin
 # shellcheck disable=SC2034
 ZSH_TMUX_AUTOSTART=true
+
+# zsh-autocomplete
+bindkey              '^I'         menu-complete
+bindkey "$terminfo[kcbt]" reverse-menu-complete
