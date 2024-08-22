a387f99a0e446b2c65e08b7a379d250cc03a4505
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-22 17:18:26 +0100

    Restore sorting

--- .config/zsh/aliases_general.zsh
+++ .config/zsh/aliases_general.zsh
@@ -51,18 +51,18 @@ alias lt='lsd --group-directories-first -lAhrt'
 x() {
     if [ -f "$1" ]; then
         case "$1" in
-        *.7z) 7z x "$1" ;;
-        *.Z) uncompress "$1" ;;
-        *.bz2) bunzip2 "$1" ;;
-        *.gz) gunzip "$1" ;;
-        *.rar) unrar x "$1" ;;
-        *.tar) tar xvf "$1" ;;
+        *) echo "Unable to extract '$1'" ;;
         *.tar.bz2) tar xvjf "$1" ;;
         *.tar.gz) tar xvzf "$1" ;;
+        *.bz2) bunzip2 "$1" ;;
+        *.rar) unrar x "$1" ;;
+        *.gz) gunzip "$1" ;;
+        *.tar) tar xvf "$1" ;;
         *.tbz2) tar xvjf "$1" ;;
         *.tgz) tar xvzf "$1" ;;
         *.zip) unzip "$1" ;;
-        *) echo "Unable to extract '$1'" ;;
+        *.Z) uncompress "$1" ;;
+        *.7z) 7z x "$1" ;;
         esac
     else
         echo "'$1' is not a valid file"
