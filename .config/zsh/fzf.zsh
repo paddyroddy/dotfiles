3f719d0e1d32ef49a70b5dbaff40cbd5140b5ebf
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-22 16:05:49 +0100

    `fzf` shell integration

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,15 +1,1 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-# shellcheck source=/dev/null
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-# shellcheck source=/dev/null
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
