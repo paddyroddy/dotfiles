09395ad344bee24fab3d1cda970fffd90da0001e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-08-22 16:18:21 +0100

    Group more `fzf` vars

--- .config/zsh/1_environment_variables_general.zsh
+++ .config/zsh/1_environment_variables_general.zsh
@@ -1,7 +1,3 @@
-# sharkdp/fd for junegunn/fzf
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--ansi"
-
 # Install in .local directory
 export PATH="$HOME/.local/bin:$PATH"
 
