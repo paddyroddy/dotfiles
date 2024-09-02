3f5cc36888bc84e68a3b87d78165cc2862a5f2b9
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-09-02 10:11:37 +0100

    British spelling

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,4 @@
-## Use 24bit colors instead of 256 colors in prompt highlighting. If your
-#  terminal supports true color, the following command should print 'TRUECOLOR'
+## Use 24bit colours instead of 256 colours in prompt highlighting. If your
+#  terminal supports true colour, the following command should print 'TRUECOLOR'
 #  in orange: printf "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
 c.TerminalInteractiveShell.true_color = True
