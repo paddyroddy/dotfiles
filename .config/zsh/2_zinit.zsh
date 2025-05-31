5505d2fda4fb37077c70579f183af02129e3eeaf
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-05-31 10:46:44 +0100

    Fix compinit problem

--- .config/zsh/2_zinit.zsh
+++ .config/zsh/2_zinit.zsh
@@ -1,9 +1,14 @@
+# Initialize completion system FIRST
+autoload -Uz compinit
+compinit
+
 # https://github.com/zdharma-continuum/zinit#manual
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+# Since we sourced zinit.zsh after compinit
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
@@ -13,3 +18,6 @@ zinit light Aloxaf/fzf-tab
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-syntax-highlighting
+
+# Replay cached completions
+zinit cdreplay -q
