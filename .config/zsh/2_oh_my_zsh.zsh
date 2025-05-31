3286a5562bb0ee7dff4599952d3184b20ccf451e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-05-31 09:57:47 +0100

    Remove unused plugins

--- .config/zsh/2_oh_my_zsh.zsh
+++ .config/zsh/2_oh_my_zsh.zsh
@@ -68,11 +68,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
-    aws
     git
-    kubectl
-    rust
-    tmux
     zsh-autocomplete
     zsh-autosuggestions
     zsh-syntax-highlighting
