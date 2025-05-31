0b9d6fc4b10c2b56acecc94f71748fa6f57ff15f
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-05-31 09:51:00 +0100

    Speed up `starship`

--- .config/zsh/starship.zsh
+++ .config/zsh/starship.zsh
@@ -1,1 +1,15 @@
 eval "$(starship init zsh)"
+
+SPACESHIP_PROMPT_ASYNC=true
+SPACESHIP_PROMPT_ADD_NEWLINE=true
+SPACESHIP_CHAR_SYMBOL="⚡"
+
+# Only load what you actually use
+SPACESHIP_PROMPT_ORDER=(
+    time
+    user
+    dir
+    git
+    line_sep
+    char
+)
