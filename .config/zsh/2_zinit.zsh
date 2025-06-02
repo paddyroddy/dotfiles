b1c646506b4937e0e534b202c71a1d3058c636fb
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-02 17:31:14 +0100

    Fix shellcheck

--- .config/zsh/2_zinit.zsh
+++ .config/zsh/2_zinit.zsh
@@ -1,11 +1,10 @@
-# shellcheck disable=SC2034
-# https://github.com/zdharma-continuum/zinit#manual
+# initialise zinit
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 if [[ ! -f "${ZINIT_HOME}/zinit.zsh" ]]; then
     print -P "%F{red}Installing Zinit...%f"
     mkdir -p "$(dirname "$ZINIT_HOME")"
-    git clone https://github.com/zdharma-continuum/zinit.git \
     # shellcheck disable=SC1091
+    git clone https://github.com/zdharma-continuum/zinit.git \
     "$ZINIT_HOME" && source "${ZINIT_HOME}/zinit.zsh"
 else
     # shellcheck disable=SC1091
