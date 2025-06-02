cf977c05932050fc005a75d903fddbbdd2cb309b
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-02 18:50:21 +0100

    Remove tmux vars (#65)

--- .config/zsh/1_environment_variables_general.zsh
+++ .config/zsh/1_environment_variables_general.zsh
@@ -1,10 +1,2 @@
 # Install in .local directory
 export PATH="$HOME/.local/bin:$PATH"
-
-# zsh-autosuggestions
-# shellcheck disable=SC2034
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=cyan"
-
-# tmux plugin
-# shellcheck disable=SC2034
-ZSH_TMUX_AUTOSTART=true
