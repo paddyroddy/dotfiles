f16cd82117a585cb66121fb9259f43c23f36475b
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-02 17:33:45 +0100

    Enable turbo lucid mode (#62)

--- .config/zsh/2_zinit.zsh
+++ .config/zsh/2_zinit.zsh
@@ -1,27 +1,22 @@
-# shellcheck disable=SC2034
-# Initialize completion system first
-autoload -Uz compinit
-compinit
-
-# https://github.com/zdharma-continuum/zinit#manual
+# initialise zinit
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-[ ! -d "$ZINIT_HOME" ] && mkdir -p "$(dirname "$ZINIT_HOME")"
-[ ! -d "$ZINIT_HOME"/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-# shellcheck disable=SC1091
-source "${ZINIT_HOME}/zinit.zsh"
-
-# Since we sourced zinit.zsh after compinit
-autoload -Uz _zinit
-# shellcheck disable=SC2154
-(( ${+_comps} )) && _comps[zinit]=_zinit
+if [[ ! -f "${ZINIT_HOME}/zinit.zsh" ]]; then
+    print -P "%F{red}Installing Zinit...%f"
+    mkdir -p "$(dirname "$ZINIT_HOME")"
+    # shellcheck disable=SC1091
+    git clone https://github.com/zdharma-continuum/zinit.git \
+    "$ZINIT_HOME" && source "${ZINIT_HOME}/zinit.zsh"
+else
+    # shellcheck disable=SC1091
+    source "${ZINIT_HOME}/zinit.zsh"
+fi
 
 # plugins
-zinit snippet OMZP::git
-zinit snippet OMZP::tmux
-zinit light Aloxaf/fzf-tab
-zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-syntax-highlighting
-
-# Replay cached completions
-zinit cdreplay -q
+zinit for \
+    wait lucid light-mode \
+    as"light" Aloxaf/fzf-tab \
+    as"light" zdharma-continuum/fast-syntax-highlighting \
+    as"light" zsh-users/zsh-autosuggestions \
+    as"snippet" OMZP::git \
+    as"snippet" OMZP::tmux \
+    as"light" atinit"zicompinit; zicdreplay -q" zsh-users/zsh-completions
