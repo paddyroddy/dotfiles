8219cb14d4ee16e274988cd758e43858a083901c
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-02 21:24:41 +0100

    History

---
+++ .config/zsh/history.zsh
@@ -0,0 +1,11 @@
+HISTDUP=erase
+HISTFILE=~/.zsh_history
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+setopt appendhistory
+setopt hist_find_no_dups
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_save_no_dups
+setopt sharehistory
