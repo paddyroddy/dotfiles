b4888e5817674596e3c7d5631f4aae845f305b2b
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-04 18:26:16 +0100

    Fix bug

--- .config/zsh/2_antidote.zsh
+++ .config/zsh/2_antidote.zsh
@@ -1,19 +1,2 @@
-# Set the root name of the plugins files (.txt and .zsh) antidote will use.
-zsh_plugins="${ZDOTDIR:-~}/.config/zsh_plugins"
-
-# Ensure the .zsh_plugins.txt file exists so you can add plugins.
-[[ -f ${zsh_plugins}.txt ]] || touch "${zsh_plugins}".txt
-
-# Lazy-load antidote from its functions directory.
-# shellcheck disable=SC2207
-fpath=($(brew --prefix)/opt/antidote/share/antidote/functions "$fpath")
-autoload -Uz antidote
-
-# Generate a new static file whenever .zsh_plugins.txt is updated.
-if [[ ! "${zsh_plugins}.zsh" -nt "${zsh_plugins}.txt" ]]; then
-  antidote bundle <"${zsh_plugins}.txt" >|"${zsh_plugins}.zsh"
-fi
-
-# Source your static plugins file.
-# shellcheck disable=SC1090
-source "${zsh_plugins}.zsh"
+source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
+antidote load
