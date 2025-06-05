962e22c9682286db1b180d1cebc4969e91fb7c4c
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-05 16:11:43 +0100

    Lazy loading

--- .config/zsh/minikube.zsh
+++ .config/zsh/minikube.zsh
@@ -1,4 +1,10 @@
-if command -v kubectl >/dev/null 2>&1; then
+kubectl() {
+    if ! command -v kubectl >/dev/null 2>&1; then
+        echo "kubectl not found"
+        return 1
+    fi
+    unfunction kubectl
     # shellcheck disable=SC1090
     source <(kubectl completion zsh)
-fi
+    kubectl "$@"
+}
