8be0f28e020788e12d297736122497303a3f96da
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-05 10:50:11 +0100

    Selective loading

--- .config/zsh/minikube.zsh
+++ .config/zsh/minikube.zsh
@@ -1,2 +1,4 @@
-# shellcheck disable=SC1090
-source <(kubectl completion zsh)
+if command -v kubectl >/dev/null 2>&1; then
+    # shellcheck disable=SC1090
+    source <(kubectl completion zsh) &
+fi
