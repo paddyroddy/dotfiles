450a933bd4501304e2e7a9c1046313864c5eeb5e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-05 11:04:08 +0100

    Further improvements

--- .config/zsh/2_antidote.zsh
+++ .config/zsh/2_antidote.zsh
@@ -1,3 +1,12 @@
-# shellcheck disable=SC1091
+# Cache antidote bundles for faster loading
+export ANTIDOTE_HOME="$HOME/.cache/antidote"
 source "$HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh"
-antidote load
+
+# Only regenerate if plugins file is newer than the cache
+zsh_plugins="$HOME/.zsh_plugins.txt"
+zsh_plugins_cache="$ANTIDOTE_HOME/plugins.zsh"
+
+if [[ ! -f "$zsh_plugins_cache" || "$zsh_plugins" -nt "$zsh_plugins_cache" ]]; then
+    antidote bundle < "$zsh_plugins" > "$zsh_plugins_cache"
+fi
+source "$zsh_plugins_cache"
