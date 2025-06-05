f48493d4e4fa46b5c31a19beefaaa455c5e5a21f
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-06-05 16:08:56 +0100

    Simplify benchmarking script

--- .config/zsh/benchmark.sh
+++ .config/zsh/benchmark.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 # Create a temporary zshrc file
 TEMP_ZSHRC="/tmp/zshrc"
-
-# Required for starting profiling
-echo "zmodload zsh/zprof" > "$TEMP_ZSHRC"
+> "$TEMP_ZSHRC"
 
 # Concatenate all .zsh files from ~/.config/zsh/
 for conf in "$HOME/.config/zsh/"*.zsh; do
     if [ -f "$conf" ]; then
-        echo -e "\n### $conf\n" >> "$TEMP_ZSHRC"
+        echo -e "### $conf\n" >> "$TEMP_ZSHRC"
         cat "$conf" >> "$TEMP_ZSHRC"
+        echo >> "$TEMP_ZSHRC"
     fi
 done
-
-# Required for finishing profiling
-echo -e "\nzprof" >> "$TEMP_ZSHRC"
-
-# Run zsh with the temporary zshrc for profiling
-echo "Running zsh with zprof and temporary configuration..."
-zsh -l -i "$TEMP_ZSHRC"
