2b956a015c12a36133167e4f2475f8ca33b73e82
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-01-31 15:40:49 +0100

    Quotes

--- .config/zsh/aliases_general.zsh
+++ .config/zsh/aliases_general.zsh
@@ -1,64 +1,64 @@
 # git aliases
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gco='git checkout'
-alias gcp='git cherry-pick'
-alias gd='git diff'
-alias gl='git pull'
-alias gp='git push'
-alias gst='git status'
-alias gsta='git stash push'
-alias gstp='git stash pop'
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gco="git checkout"
+alias gcp="git cherry-pick"
+alias gd="git diff"
+alias gl="git pull"
+alias gp="git push"
+alias gst="git status"
+alias gsta="git stash push"
+alias gstp="git stash pop"
 
 # Colour aliases
-alias cat='bat'
-alias grep='grep --color'
-alias tree='tree -C'
+alias cat="bat"
+alias grep="grep --color"
+alias tree="tree -C"
 
 # Safety aliases
-alias cp='cp -iv'
-alias ln='ln -iv'
-alias mkdir='mkdir -pv'
-alias mv='mv -iv'
-alias rm='rm -iv'
-alias wget='wget -c'
+alias cp="cp -iv"
+alias ln="ln -iv"
+alias mkdir="mkdir -pv"
+alias mv="mv -iv"
+alias rm="rm -iv"
+alias wget="wget -c"
 
 # Jupyter aliases
-alias lab='jupyter lab'
-alias notebook='jupyter notebook'
+alias lab="jupyter lab"
+alias notebook="jupyter notebook"
 
 # System visualization tools
-alias df='duf'
-alias du='dust'
-alias top='glances'
+alias df="duf"
+alias du="dust"
+alias top="glances"
 
 # Neovim alias
-alias vim='nvim'
+alias vim="nvim"
 
 # Matlab alias for nodesktop mode
-alias matlab='matlab -nodesktop'
+alias matlab="matlab -nodesktop"
 
 # BFG Repo Cleaner alias
-alias bfg='java -jar $HOME/bfg/bfg.jar'
+alias bfg="java -jar $HOME/bfg/bfg.jar"
 
 # Fix TERM issues over SSH
-alias ssh='TERM=xterm-256color ssh'
+alias ssh="TERM=xterm-256color ssh"
 
 # YAML visualization with jless
-alias yless='jless --yaml'
+alias yless="jless --yaml"
 
 # lsd aliases
-alias ls='eza --all --group-directories-first --header --icons'
-alias la='eza --all --group-directories-first --header --icons --long'
-alias lt='eza --all --group-directories-first --header --icons --reverse --sort=mod'
+alias ls="eza --all --group-directories-first --header --icons"
+alias la="eza --all --group-directories-first --header --icons --long"
+alias lt="eza --all --group-directories-first --header --icons --reverse --sort=mod"
 
 # Navigation aliases
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-alias -- -='cd -'
+alias -g ...="../.."
+alias -g ....="../../.."
+alias -g .....="../../../.."
+alias -g ......="../../../../.."
+alias -- -="cd -"
 
 # Universal file decompressor
 x() {
